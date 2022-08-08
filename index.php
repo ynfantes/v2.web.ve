90afa5c7de469c793995c3cf1671970a36f59c5c
Edgar Messia <ynfantes@gmail.com>
2022-08-08 21:52:34 +0200

    Add funcionalidad sitio en mantenimiento

--- index.php
+++ index.php
@@ -3,22 +3,38 @@ require_once 'includes/constants.php';
 $rif = array();
 $accion = isset($_GET['accion']) ? $_GET['accion'] : "index";
 if ($accion=='test') {
-    $cliente = new cliente();
+    $cliente = new cliente(SMTP);
     $cliente->enviarEmailPreRegistro(23);
 //    $r = $cliente->recuperarContraSena(3,'40666184-8','ynfantes@gmail.com');
 //    var_dump($r);
 //    die();
 }
-if(session_status()  == PHP_SESSION_NONE) {
-    session_start();
+
+$mantenimiento = MANTENIMIENTO;
+$avance = 0;
+
+if (!$mantenimiento) {
+    
+    if(session_status()  == PHP_SESSION_NONE) {
+        session_start();
+    }
+    $_SESSION['state'] = md5(uniqid(rand(), TRUE));
+    $url = urlencode(ROOT.'faceauth.php');
+    require_once './g-config.php';
+    $loginUrl = $gClient->createAuthUrl();
+    
+} else {
+    $min = date("i");
+    $avance = $min * 100 / 60;
 }
 
-$_SESSION['state'] = md5(uniqid(rand(), TRUE));
-$url = urlencode(ROOT.'faceauth.php');
-require_once './g-config.php';
-$loginUrl = $gClient->createAuthUrl();
+
+
 echo $twig->render('index.html.twig',array(
-        'url'       => $url,
-        'state'     => $_SESSION['state'],
-        'loginUrl'  => $loginUrl)
+        'url'           => $url,
+        'state'         => $_SESSION['state'],
+        'loginUrl'      => $loginUrl´,
+        'mantenimiento' => $mantenimiento,
+        'avance'        => $avance
+        )
 );
