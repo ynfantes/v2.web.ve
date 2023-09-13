8ec41649a5aa789f525ab7a620b01de91d6dca6d
Edgar Messia <ynfantes@gmail.com>
2023-09-13 19:28:15 +0200

    feat: Se implementa la firma de una petición POST para recibir los avisos de cobro vía HTTP

--- enlinea/avisos/index.php
+++ enlinea/avisos/index.php
@@ -2,32 +2,38 @@
 include_once '../../includes/constants.php';
 include_once '../../includes/propietario.php';
 
-propietario::esPropietarioLogueado();
+if ($_SERVER['REQUEST_METHOD']==='POST') {
 
-$factura = new factura();
+    var_dump($_POST);
 
-$r = $factura->facturaPerteneceACliente(
-        $_GET['id'], 
-        $_SESSION['usuario']['cedula'],
-        $_SESSION['usuario']['cod_admin']);
-
-if ($r == true ) {
-    $titulo = "AC".$_GET['id'].".pdf";
-    $content="Content-type: application/pdf";
-    $url = URL_SISTEMA."/avisos/".$_GET['id'].$_SESSION['usuario']['cod_admin'].".pdf";
-    header('Content-Disposition: inline; filename="'.$titulo.'"');
-    header($content);
-    readfile($url);
-    $bitacora = new bitacora();
-    $bitacora->insertar(Array(
-        "id_sesion"     => $_SESSION['id_sesion'],
-        "id_accion"     => 2,
-        "descripcion"   => $_GET['id'],
-    ));
-    
-//    $url = URL_SISTEMA."/avisos/".$_GET['id'].".pdf";
-//    header("location:$url");
-    
 } else {
-    echo "El aviso de cobro no se puede mostrar en estos momentos.";
+    propietario::esPropietarioLogueado();
+
+    $factura = new factura();
+
+    $r = $factura->facturaPerteneceACliente(
+            $_GET['id'], 
+            $_SESSION['usuario']['cedula'],
+            $_SESSION['usuario']['cod_admin']);
+
+    if ($r == true ) {
+        $titulo = "AC".$_GET['id'].".pdf";
+        $content="Content-type: application/pdf";
+        $url = URL_SISTEMA."/avisos/".$_GET['id'].$_SESSION['usuario']['cod_admin'].".pdf";
+        header('Content-Disposition: inline; filename="'.$titulo.'"');
+        header($content);
+        readfile($url);
+        $bitacora = new bitacora();
+        $bitacora->insertar(Array(
+            "id_sesion"     => $_SESSION['id_sesion'],
+            "id_accion"     => 2,
+            "descripcion"   => $_GET['id'],
+        ));
+        
+    //    $url = URL_SISTEMA."/avisos/".$_GET['id'].".pdf";
+    //    header("location:$url");
+        
+    } else {
+        echo "El aviso de cobro no se puede mostrar en estos momentos.";
+    }
 }
